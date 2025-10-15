03f9db3fd8bbc9fec7617717f50049afeb5150cd
jonathan strid <pjstrid@outlook.com>
2025-10-15 10:47:09 +0100

    added social number to Person.java

--- src/Person.java
+++ src/Person.java
@@ -4,10 +4,18 @@ public class Person {
 
     private int age;
 
+    private String socialNumber;
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
 
+    }
+
+    public Person(String name, int age, String socialNumber) {
+        this.name = name;
+        this.age = age;
+        this.socialNumber = socialNumber;
     }
 
     public String getName() {
@@ -16,5 +24,9 @@ public class Person {
 
     public int getAge() {
         return age;
+    }
+
+    public String getSocialNumber() {
+        return socialNumber;
     }
 }
